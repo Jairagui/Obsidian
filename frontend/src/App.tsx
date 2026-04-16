0332aa789233dd6fbe0a253cd21b9deb3aba0de7
Jair Ernesto Aguilar Limon <jairernesto.aguilar@gmail.com>
2026-04-15 22:53:39 -0600

    creacion de interfaz base de Articulos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './routes/AppRouter';
 
 function App() {
-  return (
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ frontend/src/interfaces/Articulo.ts
@@ -0,0 +1,11 @@
+// src/interfaces/Articulo.ts
+// el modelod el auto autocompletado
+export interface Articulo {
+    id: number;
+    nombre: string;
+    marca: string;
+    categoria: string;
+    anio: number;
+    condicion: string;
+    precio: number;
+}
