e0f60d5a1e7fec1e90b11743a0bf3ba265371c58
Jair Ernesto Aguilar Limon <jairernesto.aguilar@gmail.com>
2026-04-15 23:46:22 -0600

    correcion comnetarios

--- frontend/src/hooks/useBoveda.ts
+++ frontend/src/hooks/useBoveda.ts
@@ -2,21 +2,21 @@
 import { useState, useEffect } from 'react';
 import type { Articulo } from '../interfaces/Articulo';
 
-// Datos estaticos (de mientras  )
+// Datos de mientars
 const inventarioInicial: Articulo[] = [
     { id: 1, nombre: "Jordan 1 Retro", marca: "Nike", categoria: "Sneakers", anio: 2015, condicion: "Nuevo", precio: 3500 },
     { id: 2, nombre: "Vader Funko Pop", marca: "Funko", categoria: "Figuras", anio: 2020, condicion: "Con caja", precio: 600 },
     { id: 3, nombre: "Casio G-Shock", marca: "Casio", categoria: "Relojes", anio: 2018, condicion: "Usado", precio: 2200 }
 ];
 
-// manejos de etados
+// manejos de estados
 export const useBoveda = () => {
     const [articulos, setArticulos] = useState<Articulo[]>([]);
     const [busqueda, setBusqueda] = useState('');
     const [categoriaSelect, setCategoriaSelect] = useState('Todas');
     const [estaCargando, setEstaCargando] = useState(true);
 
-    // Simulamos que carga el API
+    // api momentanea
     useEffect(() => {
         setEstaCargando(true);
         setTimeout(() => {
@@ -25,7 +25,7 @@ export const useBoveda = () => {
         }, 1500);
     }, []);
 
-    // Funcion sencilla para eliminar usando filter
+    // Funcion para eliminar filtro  juanpa
     const borrarArticulo = (id: number) => {
         const listaActualizada = articulos.filter(item => item.id !== id);
         setArticulos(listaActualizada);
@@ -33,7 +33,7 @@ export const useBoveda = () => {
 
     // Buscador y filtros combinados
     const articulosFiltrados = articulos.filter((item) => {
-        // Revisamos si el nombre tiene las letras que escribieron
+        // aqui junap vemos si si esta alguna letra que escrbio el usaruio
         const coincideNombre = item.nombre.toLowerCase().includes(busqueda.toLowerCase());
         // Revisamos si la categoria es la misma que la del select
         const coincideCategoria = categoriaSelect === 'Todas' || item.categoria === categoriaSelect;
@@ -41,7 +41,7 @@ export const useBoveda = () => {
         return coincideNombre && coincideCategoria;
     });
 
-    // Sumamos los precios usando u
+    // Sumamos los precios usando
     const totalEstimado = articulosFiltrados.reduce((acumulado, item) => acumulado + item.precio, 0);
 
     return {
