3c81e5974e3347c555a050b3e8272a2a11ba9d6f
juanpabloglez25 <juan.gonzalezm@iteso.mx>
2026-04-29 20:12:46 -0600

    feat (auth): implementé autenticación del backend con registro y login usando JWT y MongoDB

---
+++ backend/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI as string)
+    .then(() => console.log("Mongo conectado"))
+    .catch(err => console.log(err));
+
+app.listen(3000, () => console.log("Servidor listo"));
